67a7755efd2aa64443a06e4f4c445814e851de4a
Yongkyun Lee <ylee2@caltech.edu>
2021-02-15 00:08:52 +0900

    Updated chat window UI

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -2,33 +2,33 @@ import { Component, OnInit } from "@angular/core";
 import { ChatService } from "src/app/services/chat.service";
 
 @Component({
-  selector: "app-chat",
-  templateUrl: "./chat.component.html",
-  styles: []
+    selector: "app-chat",
+    templateUrl: "./chat.component.html",
+    styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  message: string = "";
-  element: any;
+    message: string = "";
+    element: any;
 
-  constructor(public _chatService: ChatService) {
-    this._chatService.loadMessage().subscribe(() => {
-      setTimeout(() => {
-        this.element.scrollTop = this.element.scrollHeight;
-      }, 20);
-    });
-  }
-
-  ngOnInit() {
-    this.element = document.getElementById("app-messages");
-  }
-
-  sendMessage() {
-    if (this.message.length === 0) {
-      return;
+    constructor(public _chatService: ChatService) {
+        this._chatService.loadMessage().subscribe(() => {
+            setTimeout(() => {
+                this.element.scrollTop = this.element.scrollHeight;
+            }, 20);
+        });
     }
-    this._chatService
-      .addMessage(this.message)
-      .then(() => (this.message = ""))
-      .catch(error => console.log("error", error));
-  }
+
+    ngOnInit() {
+        this.element = document.getElementById("app-messages");
+    }
+
+    sendMessage() {
+        if (this.message.length === 0) {
+            return;
+        }
+        this._chatService
+            .addMessage(this.message)
+            .then(() => (this.message = ""))
+            .catch(error => console.log("error", error));
+    }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -15,40 +15,40 @@ import { CHATS_COLLECTION } from '../constants';
   providedIn: "root"
 })
 export class ChatService {
-  private itemsCollection: AngularFirestoreCollection<Message>;
-  public user: any = {};
-  public chats: Message[] = [];
-  // made from uid, frienduid appended in alpha order
-  public roomId: string;
-  // if the chat window is (should be) open
-  public inchat: boolean;
-  // uid of the person currently talking to
-  public friend_uid: string;
+    private itemsCollection: AngularFirestoreCollection<Message>;
+    public user: any = {};
+    public chats: Message[] = [];
+    // made from uid, frienduid appended in alpha order
+    public roomId: string;
+    // if the chat window is (should be) open
+    public inchat: boolean;
+    // uid of the person currently talking to
+    public friend_uid: string;
 
-  constructor(
-    private _angularFirestore: AngularFirestore,
-    public _angularFireAuth: AngularFireAuth
-  ) {
-    this._angularFireAuth.authState.subscribe(user => {
-      if (!user) {
-        this.user = {};
-        // if user logged out:        
-        // this._router.navigate(["/"]);
-        return;
-      }
-      // this._router.navigate(["/"]);
-      this.user.name = user.displayName;
-      this.user.uid = user.uid;
-      this.user.photo = user.photoURL;
-      // TODO ask db for the 
-      this.roomId = "lobby";
-      this.inchat = false;
-    });
-  }
+    constructor(
+        private _angularFirestore: AngularFirestore,
+        public _angularFireAuth: AngularFireAuth
+    ) {
+        this._angularFireAuth.authState.subscribe(user => {
+            if (!user) {
+                this.user = {};
+                // if user logged out:        
+                // this._router.navigate(["/"]);
+                return;
+            }
+            // this._router.navigate(["/"]);
+            this.user.name = user.displayName;
+            this.user.uid = user.uid;
+            this.user.photo = user.photoURL;
+            // TODO ask db for the 
+            this.roomId = "lobby";
+            this.inchat = false;
+        });
+    }
 
   
-  // below login/logout not used, left to keep login components alive in case
-  login(provider: string) {
+    // below login/logout not used, left to keep login components alive in case
+    login(provider: string) {
     // switch (provider) {
     //   case "google":
         this._angularFireAuth.signInWithPopup(
@@ -66,58 +66,58 @@ export class ChatService {
     //     );
     //     break;
     // }
-  }
+    }
 
-  logout() {
-    this._angularFireAuth.signOut();
-  }
+    logout() {
+        this._angularFireAuth.signOut();
+    }
 
-  loadMessage() {
-    this.itemsCollection = this._angularFirestore.collection<Message>(
-      CHATS_COLLECTION,
-      ref => ref.where("roomId", "==", this.roomId).orderBy("date", "desc").limit(10)
-    );
-    return this.itemsCollection.valueChanges().pipe(
-      map((messages: Message[]) => {
-        this.chats = [];
-        for (let message of messages) {
-          this.chats.unshift(message);
-        }
-        return this.chats;
-      })
-    );
-  }
+    loadMessage() {
+        this.itemsCollection = this._angularFirestore.collection<Message>(
+            CHATS_COLLECTION,
+            ref => ref.where("roomId", "==", this.roomId).orderBy("date", "desc").limit(10)
+        );
+        return this.itemsCollection.valueChanges().pipe(
+            map((messages: Message[]) => {
+                this.chats = [];
+                for (let message of messages) {
+                    this.chats.unshift(message);
+                }
+                return this.chats;
+            })
+        );
+    }
 
-  addMessage(text: string) {
-    let message: Message = {
-      name: this.user.name,
-      message: text,
-      date: firebase.firestore.Timestamp.now().toMillis(),
-      from_uid: this.user.uid,
-      to_uid: this.friend_uid,
-      roomId: this.roomId
-    };
-    return this.itemsCollection.add(message);
-  }
+    addMessage(text: string) {
+        let message: Message = {
+            name: this.user.name,
+            message: text,
+            date: firebase.firestore.Timestamp.now().toMillis(),
+            from_uid: this.user.uid,
+            to_uid: this.friend_uid,
+            roomId: this.roomId
+        };
+        return this.itemsCollection.add(message);
+    }
 
-  // TODO for getting unread messages
-  // getUnreads() {
-  //   return this._angularFirestore.collection(CHATS_COLLECTION,
-  //       ref => ref.where('to_uid', '==', this.user.uid))
-  //   .snapshotChanges()
-  //   .pipe(
-  //     map(actions => {
-  //         return actions.map(a => {
-  //             const message: any = a.payload.doc.data();
-  //             if (this.user.uid !== a.payload.doc.id) {
-  //                 return {
-  //                     uid: a.payload.doc.id,
-  //                     ...message
-  //                 };
-  //             }
-  //         });
-  //     })
-  //   )
-  // }
+    // TODO for getting unread messages
+    // getUnreads() {
+    //   return this._angularFirestore.collection(CHATS_COLLECTION,
+    //       ref => ref.where('to_uid', '==', this.user.uid))
+    //   .snapshotChanges()
+    //   .pipe(
+    //     map(actions => {
+    //         return actions.map(a => {
+    //             const message: any = a.payload.doc.data();
+    //             if (this.user.uid !== a.payload.doc.id) {
+    //                 return {
+    //                     uid: a.payload.doc.id,
+    //                     ...message
+    //                 };
+    //             }
+    //         });
+    //     })
+    //   )
+    // }
 
 }
